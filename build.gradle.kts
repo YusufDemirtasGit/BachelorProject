54a78e09fe500fd09030baeb76e0586293d40679
Yusuf Demirtas <138989861+YusufDemirtasGit@users.noreply.github.com>
2025-06-24 05:33:32 +0200

    Update build.gradle.kts
    just adding necessary lines for the snapshot naming

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     id("java")
 }
 
+val timestamp = project.findProperty("TIMESTAMP") ?: "local"
+version = "1.0-${timestamp}-SNAPSHOT"
+
 group = "grammarextractor"
 version = "1.0-SNAPSHOT"
 
