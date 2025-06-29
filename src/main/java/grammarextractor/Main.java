db4d673651299552fdb03357bc5469189dd86a6a
Yusuf Demirtas <yusufdemirtas211@gmail.com>
2025-06-29 20:35:54 +0200

    Extended the functionality of main class for accommodating for creating just human readable format or just decompressing from human readable

--- src/main/java/grammarextractor/Main.java
+++ src/main/java/grammarextractor/Main.java
@@ -11,9 +11,13 @@ public class Main {
         do {
             System.out.println("\nWhich mode would you like to use?\n");
             System.out.println("1. Compress");
-            System.out.println("2. Decompress");
-            System.out.println("3. Roundtrip");
-            System.out.println("4. Extract Excerpt(Not Yet fully implemented)");
+            System.out.println("2. Compress(Gonzales) not yet implemented");
+            System.out.println("3. Decompress");
+            System.out.println("4. Decompress(Gonzales) not yet implemented");
+            System.out.println("5. Roundtrip");
+            System.out.println("6. Extract Excerpt(Not Yet fully implemented)");
+            System.out.println("7. Create human readable format");
+            System.out.println("8. Decompress Human readable format back into string");
             System.out.println("99. Exit");
 
             System.out.print("Enter your choice: ");
@@ -39,7 +43,7 @@ public class Main {
                     }
                     break;
 
-                case 2:
+                case 3:
                     System.out.println("\nPlease enter the compressed input file you would like to decompress (File name ends with .rp):");
                     Path fileToTranslate = Paths.get(scanner.nextLine());
                     ProcessBuilder builder2 = new ProcessBuilder("./decoder", fileToTranslate.toString(), "input_translated.txt");
@@ -62,25 +66,23 @@ public class Main {
                         throw new FileNotFoundException();
                     }
                     break;
-                case 3:
+                case 5:
                     System.out.println("\nRoundtrip can either be used with a randomly generated string or an input file of your choice");
                     System.out.println("1. Test with randomly generated string");
                     System.out.println("2. Test with an input file of your choice");
                     int choice2;
                     choice2 = Integer.parseInt(scanner.nextLine().trim());
-                    Path fileToTest= Paths.get("Input.txt");
+                    Path fileToTest = Paths.get("Input.txt");
                     if (choice2 == 1) {
                         System.out.println("\nHow long should the test input be");
                         int length = Integer.parseInt(scanner.nextLine().trim());
                         RandomStringGenerator.generateRandomStringToFile(length, "test_input_random.txt");
                         System.out.println("Random string generated and saved to test_input_random.txt");
                         fileToTest = Paths.get("test_input_random.txt");
-                    }
-                    else if (choice2 == 2) {
+                    } else if (choice2 == 2) {
                         System.out.println("\nEnter the input file you would like to do a whole roundtrip");
                         fileToTest = Paths.get(scanner.nextLine().trim());
-                    }
-                    else{
+                    } else {
                         System.out.println("\nInvalid choice");
                     }
                     ProcessBuilder builder3 = new ProcessBuilder("./encoder", fileToTest.toString());
@@ -93,8 +95,8 @@ public class Main {
                         System.err.println("\nEncoder failed with exit code " + exitCode3);
                     }
 
-                    Path fileToTranslate2 = Paths.get(fileToTest+".rp");
-                    ProcessBuilder builder4 = new ProcessBuilder("./decoder", fileToTranslate2.toString() , "test_translated.txt");
+                    Path fileToTranslate2 = Paths.get(fileToTest + ".rp");
+                    ProcessBuilder builder4 = new ProcessBuilder("./decoder", fileToTranslate2.toString(), "test_translated.txt");
                     builder4.inheritIO(); // Optional: should let the decoder print to console
                     Process process4 = builder4.start();
                     int exitCode4 = process4.waitFor();
@@ -113,14 +115,13 @@ public class Main {
                     } catch (FileNotFoundException e) {
                         throw new FileNotFoundException();
                     }
-                    if(areFilesEqualLineByLine(fileToTest, Paths.get("test_output.txt"))){
+                    if (areFilesEqual(fileToTest, Paths.get("test_output.txt"))) {
                         System.out.println("\nTest successful. Input and output are identical");
-                    }
-                    else {
+                    } else {
                         System.err.println("Test failed, Input and Output are not identical");
                     }
                     break;
-                case 4:  // not yet fully implemented!
+                case 6:  //Not yet fully implemented
                     System.out.println("Enter the file name of the compressed grammar:");
                     String compressedGrammarFileName = scanner.nextLine().trim();
                     System.out.println("Enter the start pos for the grammar excerpt:");
@@ -147,8 +148,43 @@ public class Main {
                         System.out.println(rule < 256 ? "'" + (char) rule + "'" : "Non-terminal: " + rule);
                     }
                     break;
+                case 7:
+                    System.out.println("\nPlease enter the input file you would like to compress:");
+                    Path fileToCompress3 = Paths.get(scanner.nextLine().trim());
+                    ProcessBuilder builder5 = new ProcessBuilder("./encoder", fileToCompress3.toString());
+                    builder5.inheritIO(); // Optional: should let the decoder print to console
+                    Process process5 = builder5.start();
+                    int exitCode5 = process5.waitFor();
+                    if (exitCode5 == 0) {
+                        System.out.println("\nInput file translated successfully. The Resulting binary file is saved as:" + fileToCompress3 + ".rp");
+                    } else {
+                        System.err.println("\nEncoder failed with exit code " + exitCode5);
+                    }
+                    Path fileToTranslate4 = Paths.get(fileToCompress3 + "rp");
+                    ProcessBuilder builder6 = new ProcessBuilder("./decoder", fileToTranslate4.toString(), "input_translated.txt");
+                    builder6.inheritIO(); // Optional: should let the decoder print to console
+                    Process process6 = builder6.start();
+                    int exitCode6 = process6.waitFor();
+                    if (exitCode6 == 0) {
+                        System.out.println("\nBinary file translated successfully. The result is saved under input_translated.txt");
+                    } else {
+                        System.err.println("\nDecoder failed with exit code " + exitCode6);
+                    }
+                    break;
+                case 8:
+                    System.out.println("\nPlease enter the human readable grammar input file you would like to decompress:");
+                    Path fileToCompress5 = Paths.get(scanner.nextLine().trim());
+                    System.out.println("\nParsing the grammar from input_translated.txt");
+                    Parser.ParsedGrammar parsedGrammar3 = Parser.parseFile(fileToCompress5);
 
-
+                    String output3 = Decompressor.decompress(parsedGrammar3);
+                    try (PrintWriter out = new PrintWriter("output_from_translated.txt")) {
+                        out.println(output3);
+                        System.out.println("\nDecompression successful. Resulting text file is saved as output_from_translated");
+                    } catch (FileNotFoundException e) {
+                        throw new FileNotFoundException();
+                    }
+                    break;
 
                 case 99:
                     System.exit(0);
@@ -162,7 +198,8 @@ public class Main {
         } while (true);
 
     }
-    public static boolean areFilesEqualLineByLine(Path file1, Path file2) throws IOException {
+
+    public static boolean areFilesEqual(Path file1, Path file2) throws IOException {
         try (BufferedReader reader1 = new BufferedReader(new FileReader(file1.toFile()));
              BufferedReader reader2 = new BufferedReader(new FileReader(file2.toFile()))) {
 
